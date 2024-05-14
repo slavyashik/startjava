09882603ab4c05f3be9b6d6877a8210d61b5b734
slavyashik <git@slavaorlov.ru>
2024-05-15 01:40:09 +0700

    работа над ошибками в калькуляторе

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -13,7 +13,11 @@ public class CalculatorTest {
             if (choice.equals("yes")) {
                 System.out.print("Введите выражение: ");
                 String inputExpression = scanner.nextLine();
-                printResult(Calculator.calculate(inputExpression), inputExpression);
+                try {
+                    printResult(Calculator.calculate(inputExpression), inputExpression);
+                } catch (RuntimeException exception) {
+                    System.out.println(exception.getMessage());
+                }
             }
 
             System.out.print("\nХотите продолжить вычисления? [yes/no]: ");
@@ -23,15 +27,12 @@ public class CalculatorTest {
 
     private static void printResult(double result, String inputExpression) {
         DecimalFormat df = new DecimalFormat("#.###");
-        try {
-            System.out.printf("%s = ", inputExpression);
-            if (result == (int) result) {
-                System.out.printf("%.0f", result);
-            } else {
-                System.out.printf("%s", df.format(result));
-            }
-        } catch (RuntimeException exception) {
-            System.out.println(exception.getMessage());
+
+        System.out.printf("%s = ", inputExpression);
+        if (result == (int) result) {
+            System.out.printf("%.0f", result);
+        } else {
+            System.out.printf("%s", df.format(result));
         }
     }
 }
