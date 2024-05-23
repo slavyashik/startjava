2712ef1edf540edf63c8b50026f554ed70ce1174
slavyashik <git@slavaorlov.ru>
2024-05-23 15:27:00 +0700

    работа над форматированием + работа над ошибками

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -1,17 +1,20 @@
 \c robots;
 
 DROP TABLE IF EXISTS jaegers;
+
 CREATE TABLE jaegers (
-    id SERIAL PRIMARY KEY,
-    model_name VARCHAR(255) NOT NULL,
-    mark VARCHAR(10) NOT NULL,
-    height DECIMAL(5, 2) NOT NULL,
-    weight DECIMAL(7, 2) NOT NULL,
-    status VARCHAR(50) NOT NULL,
-    origin VARCHAR(255) NOT NULL,
-    launch DATE NOT NULL,
-    kaiju_kills INT NOT NULL DEFAULT 0
+    PRIMARY KEY (id),
+    id         SERIAL,
+    model_name VARCHAR(255)  NOT NULL,
+    mark       VARCHAR(10)   NOT NULL,
+    height     DECIMAL(5, 2) NOT NULL,
+    weight     DECIMAL(7, 2) NOT NULL,
+    status     VARCHAR(50)   NOT NULL,
+    origin     VARCHAR(255)  NOT NULL,
+    launch     DATE          NOT NULL,
+    kaiju_kill INT           NOT NULL DEFAULT 0
 );
 
 \ir populate.sql
+
 \ir queries.sql
--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,62 +1,51 @@
--- Выведите всю таблицу
 \echo 'Вывод всей таблицы';
 SELECT *
   FROM jaegers
  ORDER BY model_name;
 
--- Отобразите только не уничтоженных роботов
 \echo 'Вывод не уничтоженных роботов';
 SELECT *
   FROM jaegers
-  WHERE status <> 'Destroyed'
+ WHERE status <> 'Destroyed'
  ORDER BY model_name;
 
--- Отобразите роботов нескольких серий: Mark-1 и Mark-4
 \echo 'Вывод роботов серии Mark-1 и Mark-4';
 SELECT *
   FROM jaegers 
-  WHERE mark IN ('Mark-1', 'Mark-4') 
+ WHERE mark IN ('Mark-1', 'Mark-4')
  ORDER BY model_name;
 
--- Отобразите всех роботов, кроме Mark-1 и Mark-4 (отсортируйте таблицу по убыванию по столбцу mark)
 \echo 'Вывод всех роботов, кроме Mark-1 и Mark-4, отсортированных по убыванию по столбцу mark';
 SELECT *
   FROM jaegers
-  WHERE mark NOT IN ('Mark-1', 'Mark-4')
+ WHERE mark NOT IN ('Mark-1', 'Mark-4')
  ORDER BY mark DESC, model_name;
 
--- Отобразите информацию о самом старом роботе
 \echo 'Вывод информации о самом старом роботе';
 SELECT *
   FROM jaegers
- ORDER BY launch
- ASC LIMIT 1;
+ ORDER BY launch LIMIT 1;
 
--- Отобразите роботов, которые уничтожили больше всех kaiju, отобразив только данные столбцов model_name, mark, launch, kaiju_kill
 \echo 'Вывод роботов, уничтоживших больше всех kaiju';
-SELECT model_name, mark, launch, kaiju_kills
+SELECT model_name, mark, launch, kaiju_kill
   FROM jaegers
- ORDER BY kaiju_kills DESC;
+ ORDER BY kaiju_kill DESC LIMIT 3;
 
--- Отобразите средний вес роботов, округлив его до трех знаков после запятой
 \echo 'Вывод среднего веса роботов';
-SELECT ROUND(AVG(weight), 3) AS avg_weight
+SELECT ROUND(AVG(weight), 3) AS average_weight
   FROM jaegers;
 
--- Увеличьте на единицу количество уничтоженных kaiju у роботов, которые до сих пор не разрушены, а затем отобразите таблицу
 \echo 'Увеличение на единицу количества уничтоженных kaiju у не уничтоженных роботов';
 UPDATE jaegers
-  SET kaiju_kills = kaiju_kills + 1
-  WHERE status <> 'Destroyed';
+   SET kaiju_kill = kaiju_kill + 1
+ WHERE status <> 'Destroyed';
 
 SELECT *
   FROM jaegers
  ORDER BY model_name;
 
--- Удалите уничтоженных роботов, а затем отобразите оставшихся
 \echo 'Удаление уничтоженных роботов и вывод оставшихся';
-DELETE
-  FROM jaegers
+DELETE FROM jaegers
  WHERE status = 'Destroyed';
 
 SELECT * 
